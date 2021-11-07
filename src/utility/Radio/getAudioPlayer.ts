073a1a20c32175240e60f7e84dbc468261c54ee3
Luca Goettle <l-goettle@gmx.de>
2021-11-07 18:32:53 +0100

    change: RadioCommand uses AudioPlayer

--- src/utility/Radio/getAudioPlayer.ts
+++ src/utility/Radio/getAudioPlayer.ts
@@ -1,4 +1,6 @@
-import { AudioPlayer } from '@discordjs/voice';
+import { AudioPlayer, AudioPlayerError, AudioPlayerStatus } from '@discordjs/voice';
+import { getSongQueue } from './getSongQueue';
+import playNextSongInQueue from './playNextSongInQueue';
 
 const players: Record<string, AudioPlayer> = {};
 
@@ -6,6 +8,29 @@ export default function getAudioPlayer(guildId: string): AudioPlayer {
   const existingPlayer = players[guildId];
   if (existingPlayer) return existingPlayer;
 
-  players[guildId] = new AudioPlayer();
-  return players[guildId];
+  const player = new AudioPlayer();
+
+  players[guildId] = player;
+
+  // on Idle wait one minute before leaving
+  player.on(AudioPlayerStatus.Idle, () => {
+    console.log('player is idle. Playing next resource');
+    const queue = getSongQueue(guildId);
+    if (queue) playNextSongInQueue(player, queue).catch((err) => console.error('Error: playing next song in idle failed: ', err));
+  });
+
+  // print message which song is now played
+  player.on(AudioPlayerStatus.Playing, () => {
+    console.log('Player is playing');
+  });
+
+  // on error play next song
+  player.on('error', (error) => {
+    if (error instanceof AudioPlayerError) console.error(`Player Error: ${error.message} with resource ${error.resource.metadata}`);
+    console.error(`Player Error: ${error.message} with resource ${error}`);
+    const queue = getSongQueue(guildId);
+    if (queue) playNextSongInQueue(player, queue).catch((err) => console.error('Error: playing next song after error failed: ', err));
+  });
+
+  return player;
 }
