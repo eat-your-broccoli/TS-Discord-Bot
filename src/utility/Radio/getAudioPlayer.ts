94f8b49c09492ee2800b32ccb555be4560be2910
Luca Goettle <l-goettle@gmx.de>
2021-11-08 13:22:07 +0100

    refactoring

--- src/utility/Radio/getAudioPlayer.ts
+++ src/utility/Radio/getAudioPlayer.ts
@@ -4,33 +4,50 @@ import playNextSongInQueue from './playNextSongInQueue';
 
 const players: Record<string, AudioPlayer> = {};
 
-export default function getAudioPlayer(guildId: string): AudioPlayer {
-  const existingPlayer = players[guildId];
-  if (existingPlayer) return existingPlayer;
-
+function createAudioPlayer(guildId: string): AudioPlayer {
   const player = new AudioPlayer();
 
   players[guildId] = player;
 
   // on Idle wait one minute before leaving
   player.on(AudioPlayerStatus.Idle, () => {
-    console.log('player is idle. Playing next resource');
+    console.log('player finished playing and is in idle');
     const queue = getSongQueue(guildId);
-    if (queue) playNextSongInQueue(player, queue).catch((err) => console.error('Error: playing next song in idle failed: ', err));
+    if (queue) {
+      console.log(`members in the voice channel: ${queue.voice.members.size}`);
+      if (queue.voice.members.size === 1) {
+        console.log('there are no active connections in the voice channel.');
+      } else playNextSongInQueue(player, queue).catch((err) => console.error('Error: playing next song in idle failed: ', err));
+    } else {
+      console.log('queue is empty. TODO close connection');
+    }
   });
 
   // print message which song is now played
   player.on(AudioPlayerStatus.Playing, () => {
-    console.log('Player is playing');
+    const queue = getSongQueue(guildId);
+    const title = queue?.currentSong?.title || 'unknown';
+    console.log(`Player is now playing: ${title}`);
   });
 
   // on error play next song
   player.on('error', (error) => {
     if (error instanceof AudioPlayerError) console.error(`Player Error: ${error.message} with resource ${error.resource.metadata}`);
-    console.error(`Player Error: ${error.message} with resource ${error}`);
+    else console.error(`Player Error: ${error}`);
     const queue = getSongQueue(guildId);
     if (queue) playNextSongInQueue(player, queue).catch((err) => console.error('Error: playing next song after error failed: ', err));
   });
 
   return player;
+}
+
+export default function getAudioPlayer(guildId: string): AudioPlayer | null {
+  return players[guildId];
+}
+
+export function getOrCreateAudioPlayer(guildId: string): AudioPlayer {
+  const existingPlayer = players[guildId];
+  if (existingPlayer) return existingPlayer;
+
+  return createAudioPlayer(guildId);
 }
