638fc3096225b1b989f6d0e6d3c20a92c30d0cd4
eat-your-broccoli <37545844+eat-your-broccoli@users.noreply.github.com>
2021-11-13 12:14:26 +0100

    Issue/#31 volume (#32)
    * implemented db for volume
    
    * change: volume change affects player
    
    * fix: don't track volume.db
    
    * feat: songs that are started use volume
    
    * change: docker volume for data
    
    * change: forbid large volume changes

---
+++ src/utility/Radio/VolumeManager.ts
@@ -0,0 +1,66 @@
+import * as sqlite from 'sqlite3';
+import { AudioPlayerPlayingState } from '@discordjs/voice';
+// eslint-disable-next-line import/no-cycle
+import getAudioPlayer from './getAudioPlayer';
+
+const table = 'volume';
+
+const db = new sqlite.Database('./data/volume/volume.db');
+const defaultVolume = 35;
+
+export default class VolumeManager {
+  public static async init(): Promise<void> {
+    db.serialize(() => {
+      db.run(`CREATE TABLE IF NOT EXISTS ${table} (guildId TEXT UNIQUE, vol INTEGER)`);
+    });
+  }
+
+  public static async set(guildId: string, volume: number): Promise<void> {
+    const oldVol = await this.get(guildId);
+    if (volume > 40 && volume > oldVol + 20) throw new Error('Volume cannot be changed by steps larger than 20');
+    await this.setInDB(guildId, volume);
+    const player = getAudioPlayer(guildId);
+    if (!player) return;
+    const newVol = this.volumeToDecimal(volume);
+    (player.state as AudioPlayerPlayingState).resource.volume.setVolume(newVol);
+  }
+
+  public static async get(guildId: string): Promise<number> {
+    const val: any = await this.getFromTable(guildId);
+    if (val == null) {
+      await this.setInDB(guildId, defaultVolume);
+      return (this.volumeToDecimal(defaultVolume));
+    }
+    return (this.volumeToDecimal(val.vol));
+  }
+
+  public static async setInDB(guildId: string, volume: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const sql = `INSERT OR REPLACE INTO ${table} (guildId, vol) VALUES(${String(guildId)}, ${Number(volume)});`;
+      db.run(sql, [], (err: Error, row: any) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        }
+        resolve(row);
+      });
+    });
+  }
+
+  private static getFromTable(guildId: string): Promise<number> {
+    return new Promise((resolve, reject) => {
+      const sql = `SELECT vol FROM ${table} WHERE guildId = ${String(guildId)}`;
+      db.get(sql, [], (err, row) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        }
+        resolve(row);
+      });
+    });
+  }
+
+  private static volumeToDecimal(vol: number) {
+    return vol / 100;
+  }
+}
